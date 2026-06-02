8e320ea78af79eed4f79880c5ff83dc376a2d989
sakshyam gautam <sakshyamgautam79@gmail.com>
2026-06-02 18:05:52 +0545

    Add files via upload

--- Todo.jsx
+++ Todo.jsx
@@ -1,17 +1,52 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png"
 import TodoItems  from "./Todoitems"
 
 const Todo = () => {
 
+  const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")) : []);
 const inputRef = useRef();
 
 const add = () => {
-const inputText = inputRef.current.value;
-console.log(inputText);
+const inputText = inputRef.current.value.trim();
+if (inputText === ""){
+  return null;
+}
+
+const newTodo = {
+  id: Date.now(),
+  text: inputText,
+  isComplete: false,
+}
+setTodoList((prev)=>[...prev, newTodo]);
+inputRef.current.value = "";
 
 
 }
+
+const deleteTodo = (id) => {
+setTodoList((prvTodos) =>{
+return  prvTodos.filter((todo)=> todo.id !== id)
+})
+
+}
+
+const toggle =(id)=>{
+  setTodoList((prevTodos)=>{
+    return prevTodos.map((todo)=>{
+      if(todo.id === id){
+        return {...todo, isComplete: !todo.isComplete}
+      }
+      return todo;
+  })
+  })
+  
+}
+
+useEffect(()=>{
+localStorage.setItem("todos", JSON.stringify(todoList))
+  
+},[todoList])
 
   return (
     <div
@@ -34,10 +69,14 @@ console.log(inputText);
 
 {/* ------ todo list  ------ */} 
 
+
 <div>
   
-<TodoItems text="SAMIPA"/>
-<TodoItems text="GYAWALI"/>
+{todoList.map((item, index)=>{
+return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
+})}
+
+
 </div>
 
 
